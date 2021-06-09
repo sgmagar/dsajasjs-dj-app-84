9b2050d52a99c3e81b7abd49c260fadf97ac236b
Saurav <sp.gharti@gmail.com>
2021-06-09 15:34:48 +0000

    Update models

--- users/api/v1/serializers.py
+++ users/api/v1/serializers.py
@@ -1,8 +1,1 @@
 from rest_framework import serializers
-from users.models import User
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        exclude = ["password"]
